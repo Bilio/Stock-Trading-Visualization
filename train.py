b755de322c37b3eac0ae53ec56c03d6b872e2fe1
draichi <lucasdraichi@gmail.com>
2019-05-07 01:02:20 -0300

    train

--- train.py
+++ train.py
@@ -17,29 +17,31 @@ from ray.tune.registry import register_env
 # from configs.vars import WALLET_FIRST_SYMBOL, WALLET_SECOND_SYMBOL
 
 if __name__ == "__main__":
-    df = pd.read_csv('./datasets/bot_train_ETHBTC_700_day.csv')
+    df = pd.read_csv('./datasets/bot_train_ETHUSDT_600_hour.csv')
+    # df = pd.read_csv('./datasets/bot_train_ETHBTC_700_day.csv')
     df = df.sort_values('Date')
     register_env("StockTradingEnv-test", lambda config: StockTradingEnv(config))
     ray.init()
     run_experiments({
-        "test3_stock_visualization": {
+        "ETHUSDT_600_hour2": {
             "run": "PPO",
             "env": "StockTradingEnv-test",
             "stop": {
-                "timesteps_total": 1e5, #1e6 = 1M
+                "timesteps_total": 1.2e6, #1e6 = 1M
             },
-            "checkpoint_freq": 50,
+            "checkpoint_freq": 100,
             "checkpoint_at_end": True,
             "config": {
                 "lr": grid_search([
-                    1e-4
-                    # 1e-6
+                    # 8e-6,
+                    5e-5
                 ]),
-                "num_workers": 2,  # parallelism
+                "num_workers": 3,  # parallelism
                 'observation_filter': 'MeanStdFilter',
-                "vf_clip_param": 100000.0, # used to trade BTCUSDT
+                # "vf_clip_param": 10000000000.0, # used to trade large rewarded with PPO
                 "env_config": {
-                    'df': df
+                    'df': df,
+                    'render_title': ''
                 }
             }
         }
